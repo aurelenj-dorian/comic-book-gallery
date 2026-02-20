e11efe4a14c6263a06db949158bc29341b63f24a
aurele dorian <aureledorian@gmail.com>
2026-02-20 08:39:00 +0100

    Ajout d'une page detail

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -8,7 +8,28 @@ namespace OnepieceBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string 
-       // return "Hello from the ComicBooksController!";
+       /* public ContentResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the ComicBooksController!");
+        }
+
+        private ContentResult Redirect(string v)
+        {
+            throw new NotImplementedException();
+        }*/
+
+        public ActionResult Detail()
+        {
+            /*if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the ComicBooksController!");*/
+            return View();
+        }
     }
 }
